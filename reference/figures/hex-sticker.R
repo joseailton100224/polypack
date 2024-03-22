ef38f0cb6ff3f8dff932505256df3b3e72c64905
joseailton100224 <159630950+joseailton100224@users.noreply.github.com>
2024-03-22 00:05:32 +0000

    Deploying to gh-pages from @ joseailton100224/polypack@42d0479a30a92dbfbbcc7e81a144d4b3d7b24e7b 🚀

---
+++ reference/figures/hex-sticker.R
@@ -0,0 +1,8 @@
+p <- "man/figures/logo2.png"
+
+hexSticker::sticker(
+  p, package="polypack",p_color = "#000000", p_size=20,
+  s_x=1, s_y=.75, s_width=0.75,s_height = 0.75,
+  h_fill="#F5F611", h_color="#000000",
+  filename="man/figures/logo.png"
+)
